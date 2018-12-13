afbb6eeb86a29088dbe390caa8394d9e8ace241f
Manuel Carrizo <manuelcarrizo@gmail.com>
2018-12-13 13:30:08 -0300

    Changed description

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='vbox-cli',
-    version='0.0.1',
-    description='Wrapper for VBoxManage to list, start, stop and attach/detach ISOs',
+    version='0.0.2',
+    description='Wrapper for VBoxManage to list, start and stop VMs. Also can attach/detach ISOs',
     url='https://github.com/manuelcarrizo/vbox',
     author='Manuel Carrizo',
     author_email='manuelcarrizo@gmail.com',
